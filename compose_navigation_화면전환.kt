c80a0b14d9cb6c9f865943c1e5c810a9d6e55c2b
준호 <139526084+junnamu@users.noreply.github.com>
2023-08-16 11:27:09 +0900

    Update compose_navigation_화면전환.kt

--- compose_navigation_화면전환.kt
+++ compose_navigation_화면전환.kt
@@ -1,6 +1,9 @@
-// build.gradle module에 아래 dependencis에 아래 코드 추가  / 특히 아래 코드는 rememberNavController() , composable() 추가를 위해서 필요
+// build.gradle module에 아래 dependencis에 아래 코드 추가  / 아래 특정 코드는 rememberNavController() , composable() 추가를 위해서 필요
+// 
+
 //   val nav_version = "2.5.3"
 //   implementation("androidx.navigation:navigation-compose:$nav_version")
-//   implementation("androidx.navigation:navigation-fragment:2.4.1")
-//   implementation("androidx.navigation:navigation-compose:2.4.1")
-//
+
+//   implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+//   implementation("com.attafitamim.navigation:android:1.0.0-beta01")
+//   implementation("androidx.navigation:navigation-runtime-ktx:2.5.3")
