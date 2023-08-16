36d5a35c6d52d21b6c02874d93243bef8d1cd1c3
준호 <139526084+junnamu@users.noreply.github.com>
2023-08-16 11:11:12 +0900

    Create compose_navigation_화면전환.kt

---
+++ compose_navigation_화면전환.kt
@@ -0,0 +1,6 @@
+// build.gradle module에 아래 dependencis에 아래 코드 추가  / 특히 아래 코드는 rememberNavController() , composable() 추가를 위해서 필요
+//   val nav_version = "2.5.3"
+//   implementation("androidx.navigation:navigation-compose:$nav_version")
+//   implementation("androidx.navigation:navigation-fragment:2.4.1")
+//   implementation("androidx.navigation:navigation-compose:2.4.1")
+//
